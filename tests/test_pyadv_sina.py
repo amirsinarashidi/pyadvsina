ccfe12da85d88f031498dd0fb8b243f69a4249ea
Amirsina <amirsina.rashidi71@gmail.com>
2024-06-10 14:40:10 +0200

    add test

--- tests/test_pyadv_sina.py
+++ tests/test_pyadv_sina.py
@@ -1,2 +1,12 @@
 def test_something():
     pass
+
+from pyadv_sina import algos
+    
+def test_add_one():   
+    out = algos.add_one(1)
+    assert out == 2
+
+def test_add_two():   
+    out = algos.add_two(1)
+    assert out == 3
